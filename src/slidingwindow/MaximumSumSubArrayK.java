50f65501957343d6df25dad8c815854d9068d48b
Ashwin Kushwah <ashwinkushwah456@gmail.com>
2022-03-14 13:11:24 +0530

    Added Maximum Sum SubArray of Size K

---
+++ src/slidingwindow/MaximumSumSubArrayK.java
@@ -0,0 +1,29 @@
+package slidingwindow;
+
+public class MaximumSumSubArrayK {
+   public static Integer maximumSumSubArray(int K,int [] elements){
+       int maxSum = 0 , windowStart = 0 ;
+       int subArraySum = 0;
+       for(int windowEnd = 0; windowEnd < elements.length; windowEnd++){
+             subArraySum += elements[windowEnd];
+
+             if(windowEnd >= K-1){
+                maxSum = Math.max(subArraySum, maxSum);
+                subArraySum -= elements[windowStart];
+                windowStart++;
+             }
+
+       }
+
+       return maxSum;
+   }
+
+   public static void main(String [] args){
+       System.out.println("Maximum Sum of SubArray = " +
+        maximumSumSubArray(3, new int[]{2,1,5,1,3,2}) );
+
+        System.out.println("Maximum Sum of SubArray = " +
+        maximumSumSubArray(2, new int[]{2,3,4,1,5}) );
+        
+   }
+}
