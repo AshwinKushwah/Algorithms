15c6a6222338adc0db410daa3ab5a8f8cfe4b3a3
Ashwin Kushwah <ashwinkushwah456@gmail.com>
2022-04-02 22:41:13 +0530

    Added Complexity Info for Consecutive Ones

--- src/slidingwindow/LongestSubarrayWithOnesAfterReplacement.java
+++ src/slidingwindow/LongestSubarrayWithOnesAfterReplacement.java
@@ -4,6 +4,9 @@
  * you may change up to K values from 0 to 1. 
  * Return the length of the longest (contiguous) subarray that contains only 1’s. */
 
+//Time Complexity - O(N)
+//Space Complexity - O(1)
+ 
 package slidingwindow;
 
 public class LongestSubarrayWithOnesAfterReplacement {
